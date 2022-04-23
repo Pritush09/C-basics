2afa8b4752e6c2d020edfd2fcff5e84dc8613ab5
Pritush Singh <mynameispritush.15@gmail.com>
2022-04-23 13:11:36 +0530

    done with the remaining part

--- testing.c
+++ testing.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    float arr[] = {12.4,2.3,4.5,6.7};
-    printf("%d\n",sizeof(arr)/sizeof(arr[0]));
+    int b = 1;
+    printf("%d",sizeof(b));
+    return 0;
 }
- 
- 
-
